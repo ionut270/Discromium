6327a4e65a2ddd267af799896ba138f7e1363ed9
Ionut <oanceaionut15@gmail.com>
2024-06-28 20:00:05 +0300

    custom port

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 80;
+const port = 7777;
 
-// Serve static files from the /client directory
 app.use(express.static(path.join(__dirname, 'client')));
 
 app.get('/', (req, res) => {
